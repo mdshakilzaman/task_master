5820037bd4f71e253efe43b1494788ba0f1075f4
mz1482 <mz1482@rit.edu>
2022-08-14 14:05:54 -0400

    basic signup

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,46 @@
+from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def home(request):
-    return HttpResponse("hello I am working")
+    return render(request,"authentication/index.html")
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.save()
+        messages.success(request,"your account has been successfully created")
+        return redirect('signin')
+
+
+    return render(request,"authentication/signup.html")
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username = username, password = pass1)
+
+        if user is not None:
+            username = user.username
+            login(request, user)
+            return render(request,"authentication/index.html",{'username':username})
+        else:
+            messages.error(request, "Bad credentials")
+            return redirect('home')
+    return render(request,"authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request,"logged out successfully")
+    return redirect('home')
--- task_master/settings.py
+++ task_master/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'task_master.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
